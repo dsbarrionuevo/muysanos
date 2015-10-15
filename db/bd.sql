a4f75a1aab3a2b6eeb0f409b711965ce80d9967b
Diego Barrionuevo <dsbarrionuevo@hotmail.com>
2015-10-14 22:58:56 -0300

    Commit inicial. Base de datos.

---
+++ db/bd.sql
@@ -0,0 +1,165 @@
+DROP DATABASE IF EXISTS muysanos;
+CREATE DATABASE IF NOT EXISTS muysanos;
+USE muysanos;
+
+CREATE TABLE tipos_frecuencias(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE tipos_comidas(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE tipos_unidades(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE familias(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE alimentos(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    id_familia INT NOT NULL,
+    id_tipo_unidad INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_familia) REFERENCES familias(id),
+    FOREIGN KEY(id_tipo_unidad) REFERENCES tipos_unidades(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE alimentos_patron(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_alimento INT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_alimento) REFERENCES alimentos(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE equivalencias_alimentos(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_alimento_patron INT NOT NULL,
+    id_alimento INT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_alimento_patron) REFERENCES alimentos_patron(id),
+    FOREIGN KEY(id_alimento) REFERENCES alimentos(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE dietas(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    fecha_creacion DATE NULL,
+    PRIMARY KEY(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE alimentos_x_dieta(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_dieta INT NOT NULL,
+    id_alimento_patron INT NOT NULL,
+    id_tipo_frecuencia INT NOT NULL,
+    cantidad INT NOT NULL, /* cantidad de veces que está la cantidad definia en patrón */
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_dieta) REFERENCES dietas(id),
+    FOREIGN KEY(id_alimento_patron) REFERENCES alimentos_patron(id),
+    FOREIGN KEY(id_tipo_frecuencia) REFERENCES tipos_frecuencias(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE platos(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    descripcion TEXT NULL,
+    preparacion TEXT NULL,
+    valoracion DECIMAL(2,1) NULL,
+    dificultad DECIMAL(2,1) NULL,
+    porciones INT NULL,
+    fecha_creacion DATE NULL,
+    PRIMARY KEY(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE alimentos_x_plato(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_plato INT NOT NULL,
+    id_alimento INT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_plato) REFERENCES platos(id),
+    FOREIGN KEY(id_alimento) REFERENCES alimentos(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE pacientes(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE NULL,
+    /* peso y estatura se actualizarian a lo largo de mucho tiempo... */
+    peso DECIMAL(4,2) NOT NULL,
+    estatura DECIMAL(4,2) NOT NULL,
+    PRIMARY KEY(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE comidas_x_paciente(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_paciente INT NOT NULL,
+    id_tipo_comida INT NOT NULL,
+    fecha_hora DATETIME NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_paciente) REFERENCES pacientes(id),
+    FOREIGN KEY(id_tipo_comida) REFERENCES tipos_comidas(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE platos_x_paciente(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_comida INT NOT NULL,
+    id_plato INT NOT NULL,
+    porciones INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_comida) REFERENCES comidas_x_paciente(id),
+    FOREIGN KEY(id_plato) REFERENCES platos(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE alimentos_x_paciente(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_comida INT NOT NULL,
+    id_alimento INT NOT NULL,
+    cantidad INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_comida) REFERENCES comidas_x_paciente(id),
+    FOREIGN KEY(id_alimento) REFERENCES alimentos(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE dietas_x_paciente(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_paciente INT NOT NULL,
+    id_dieta INT NOT NULL,
+    fecha_inicio DATE NOT NULL,
+    peso_inicial DECIMAL(4,2) NOT NULL,
+    estatura_inicial DECIMAL(4,2) NOT NULL,
+    peso_deseado DECIMAL(4,2) NOT NULL,
+    fecha_fin DATE NULL,
+    peso_final DECIMAL(4,2) NULL,
+    estatura_final DECIMAL(4,2) NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_paciente) REFERENCES pacientes(id),
+    FOREIGN KEY(id_dieta) REFERENCES dietas(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE TABLE historial_dieta_paciente(
+    id INT NOT NULL AUTO_INCREMENT,
+    id_dieta_paciente INT NOT NULL,
+    nro_semana INT NULL,
+    fecha DATE NOT NULL,
+    peso DECIMAL(4,2) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(id_dieta_paciente) REFERENCES dietas_x_paciente(id)
+)ENGINE InnoDB DEFAULT CHARACTER SET=utf8;
+
+CREATE USER 'muysanos_user'@'localhost' IDENTIFIED BY 'ms_112233';
+GRANT SELECT, INSERT, UPDATE ON muysanos.* TO 'muysanos_user'@'localhost';
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="windows-1252">
+        <title>Muy Sanos</title>
+        <link href="css/estilo.css" type="text/css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div id="contenedor">
+            <header></header>
+            <main></main>
+            <footer></footer>
+        </div>
+    </body>
+</html>
